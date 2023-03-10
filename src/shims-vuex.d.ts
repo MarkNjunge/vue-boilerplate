31bd43d3de192628820349f419b58acd6696e6e2
MarkNjunge <mark.kamau@outlook.com>
2023-03-10 23:41:55 +0300

    Remove unused type declarations

--- src/shims-vuex.d.ts
+++
@@ -1,8 +0,0 @@
-import { Store } from "vuex";
-import { Store as AppStore } from "@/store";
-
-declare module "@vue/runtime-core" {
-  interface ComponentCustomProperties {
-    $store: Store<typeof AppStore>;
-  }
-}
