31f20ac29551f9083ad3d309b98cb7bdb058f3f2
MarkNjunge <mark.kamau@outlook.com>
2023-03-11 15:33:23 +0300

    Add functions to create banners

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,9 +2,21 @@ import store from "@/store";
 
 export function errorHandler(error: Error) {
   console.log(error.message);
+  errorBanner(error.message);
+}
+
+export function infoBanner(message: string, timeout: number = 3000) {
   store.commit("ui/addBanner", {
-    type: "error",
-    message: error.message,
-    timeout: 0,
+    type: "info",
+    message,
+    timeout,
+  });
+}
+
+export function errorBanner(message: string, timeout: number = 3000) {
+  store.commit("ui/addBanner", {
+    type: "info",
+    message,
+    timeout,
   });
 }
